f52351e03d31157a897dd1b53cdef7a67273add2
Nkuryant <45668645+Nkuryant@users.noreply.github.com>
2018-12-18 11:25:17 +0300

    Update module.py

--- python/module.py
+++ python/module.py
@@ -1,36 +1,50 @@
 def add_user():
-    flag_1=True
+    active=True
     censor_name="""1234567890!@#$%^\"&*()_+\|/№;%:?  """
-    flag_user=True
-    while flag_user:        
-            flag_1=True
-            name=str(input("Введите имя:"))
+    while active:
+        correct_input=True        
+        while correct_input:        
+            name=str(input("Введите ваше имя:"))
             name=name.strip()
+            name=name.title()
             if name.lower() == "выход":
+                active=False
+                correct_input=False
                 break
-            if len(name)>1 and len(name) < 16:
-                for letter in name:
-                    if letter in censor_name:
-                        print("Ошибка ввода имени")
-                        flag_1=False
-                        break
-                if flag_1 ==  True:
-                    name=name.title()
-                    age=str(input("Введите ваш возраст:"))
-                    if age == "выход":
-                        break
-                    elif age.isdigit():
-                        if int(age) in range(0,121):
-                            with open("users.txt","a") as file_obj:
-                                file_obj.write("<name=%s:age=%s>\n"%(name,age))
-                                print("Пользователь добавлен")
-                                flag_user=False
-                        else:
-                            print("Ошибка ввода возраста")
-                    else:
-                        print("Ошибка ввода возраста")
+            for letter in censor_name:
+                if name.find(letter)>-1:
+                    print("Недопустимый символ в имени")
+                    correct_input=False
+                    break
+            if len(name)<=1:
+                print("Короткое имя ¯\_(0_0)_/¯")
+                correct_input=False
+            elif len(name)>16:
+                print("Слишком длинное имя ¯\_(0_0)_/¯")
+                correct_input=False
             else:
-                print("ошибка ввода имени")
+                break                   
+        while correct_input:
+            age=str(input("Введите ваш возраст:"))
+            if age.lower() == "выход":
+                active=False
+                correct_input=False
+                break
+            if age.isdigit():
+                if int(age) not in range(0,121):
+                    print("Некоректный возраст")
+                    correct_input=False
+                else:
+                    break
+            else:
+                print("Некоректное число")
+                correct_input=False
+        if correct_input:
+            with open("users.txt","a") as file_obj:
+                file_obj.write("<name=%s:age=%s>\n"%(name,age))
+                print("Пользователь добавлен")
+                active=False
+                
 def watch_users():    
     with open("users.txt") as file_obj:
         all_users=file_obj.readlines()
@@ -53,10 +67,12 @@ def watch_users():
             for i in range(len(searching_users)):
                 curr_row=searching_users[i]
                 print("Пользователь %s, возраст %s лет"%(curr_row[6:curr_row.find(":")],curr_row[curr_row.find(":")+5:curr_row.find(">")]))
-                
+    else:
+        print("Такой команды нет")
 def show_instruction():
             print("""\nДля того чтобы добавить в базу пользователя - напишите 'добавить'
 Для просмотра пользователей напишите 'просмотр'
 Для выхода из команд бота напишите 'выход'
               """)
+
 
